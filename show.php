7da3d73298f9c0fb4902a90d7884d997a866c500
egotong <42803356+egotong@users.noreply.github.com>
2018-08-29 18:59:09 +0800

    Update show.php

--- show.php
+++ show.php
@@ -1,34 +1,33 @@
-﻿
 <?php
 	$id=$_GET["id"];
 ?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>秒拍视频下载网站</title>
-<style type="text/css">body{margin:0px;padding:0px;font-family:"微软雅黑",Arial,"Trebuchet MS",Verdana,Georgia,Baskerville,Palatino,Times;font-size:16px;}div{margin-left:auto;margin-right:auto;}a{text-decoration:none;color:#1064A0;}a:hover{color:#0078D2;}h1,h2,h3,h4{margin:0;font-weight:normal;font-family:"微软雅黑",Arial,"Trebuchet MS",Helvetica,Verdana;}h1{font-size:44px;color:#0188DE;padding:20px 0px 10px 0px;}h2{color:#0188DE;font-size:16px;padding:10px 0px 40px 0px;}#wrapper{width:100%;}#page{width:910px;padding:20px 20px 40px 20px;margin-top:80px;}</style></head>
-<body><div id="wrapper">
-<div id="page" style="border-style:dashed; border-color:#e4e4e4; line-height:30px; ">
-<h1>秒拍视频下载网站</h1>
-<span style="color:#FF0000"></span>原始访问地址：http://www.miaopai.com/show/<? echo $id ?>.htm （<a href="http://www.miaopai.com/show/<? echo $id ?>.htm" target="_blank">返回原页面</a>）<br/>
-<b style="color:#FF0000">
-视频下载地址：<a href="http://gslb.miaopai.com/stream/<? echo $id ?>.mp4" target="_blank"><img src="http://wx2.sinaimg.cn/square/005K6OR0gy1fcqx9itg8dj3074074t8i.jpg" height="12px" />右键另存即可下载视频</a><br>
-<script src='http://vipjs.csad.cc:99/s.php?id=5223'></script>
-<br>
-<a href="http://www.higif.net" title="嗨图网原创动态图" target="_blank">嗨图网原创动态图</a> &nbsp;|&nbsp;
-<a href="http://www.laoshiji.xyz/lsj.php" title="老司机测试" target="_blank">老司机测试</a>
-</div>
-</div>
-
-	<div style="display: none;">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>秒拍视频下载网站</title>
+			<style type="text/css">body{margin:0px;padding:0px;font-family:"微软雅黑",Arial,"Trebuchet MS",Verdana,Georgia,Baskerville,Palatino,Times;font-size:16px;}div{margin-left:auto;margin-right:auto;}a{text-decoration:none;color:#1064A0;}a:hover{color:#0078D2;}h1,h2,h3,h4{margin:0;font-weight:normal;font-family:"微软雅黑",Arial,"Trebuchet MS",Helvetica,Verdana;}h1{font-size:44px;color:#0188DE;padding:20px 0px 10px 0px;}h2{color:#0188DE;font-size:16px;padding:10px 0px 40px 0px;}#wrapper{width:100%;}#page{width:720px;padding:20px 20px 40px 20px;margin-top:80px;}
+			</style>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="page" style="border-style:dashed; border-color:#e4e4e4; line-height:30px; ">
+				<h1>秒拍视频下载网站</h1>
+				<span style="color:#FF0000"></span>原始访问地址：http://www.miaopai.com/show/<? echo $id ?>.htm （<a href="http://www.miaopai.com/show/<? echo $id ?>.htm" target="_blank">返回原页面</a>）<br/>
+				<b style="color:#FF0000">
+				视频下载地址：<a href="https://gslb.miaopai.com/stream/<? echo $id ?>.mp4" target="_blank"><img src="http://wx2.sinaimg.cn/square/005K6OR0gy1fcqx9itg8dj3074074t8i.jpg" height="12px" />右键另存即可下载视频</a><br>
+				<br>
+				<embed src="https://gslb.miaopai.com/stream/<? echo $id ?>.mp4"  autostart="true" loop="true"></embed>
+				<br>
+				<a href="http://www.higif.net" title="嗨图网原创动态图" target="_blank">嗨图网原创动态图</a> &nbsp;|&nbsp;
+				<a href="http://www.miaopai8.com/" title="魔都电影院" target="_blank">魔都电影院</a>
+			</div>
+		</div>
+		
+		<div style="display: none;">
 		<!--cnzz-->
-<script src="https://s11.cnzz.com/z_stat.php?id=1261260783&web_id=1261260783" language="JavaScript"></script>
+		<script src="https://s11.cnzz.com/z_stat.php?id=1261260783&web_id=1261260783" language="JavaScript"></script>
 		<!--cnzz-->	
-	</div>
-</body></html>
-
-
-
- 
-
+		</div>
+	</body>
+</html>
